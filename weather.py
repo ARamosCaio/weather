b905a4847ae28b1de8bee34b912f7b655c4a6528
Caio Ramos <aramoscaio@gmail.com>
2023-06-19 23:03:03 -0300

    fix: fixed typos

--- weather.py
+++ weather.py
@@ -15,13 +15,14 @@ class WeatherData:
 
 def get_coordinates(city_name, state_code, country_code, API_KEY):
     response = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_KEY}').json()
-
+    
     data = response[0]
     lat, lon = data.get('lat'), data.get('lon')
 
     return lat, lon
 
 def get_current_weather(lat, lon, api_key):
+    # sourcery skip: inline-immediately-returned-variable
     response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric').json()
     data = WeatherData(
         main=response.get('weather')[0].get('main'),
@@ -32,7 +33,8 @@ def get_current_weather(lat, lon, api_key):
     return data
 
 def main(city_name, state_name, country_name):
-    lat, lon = get_coordinates('Toronto', 'ON', 'Canada', api_key)
+    # sourcery skip: inline-immediately-returned-variable
+    lat, lon = get_coordinates(city_name, state_name, country_name, api_key)
     weather_data = get_current_weather(lat, lon, api_key)
     return weather_data
 
